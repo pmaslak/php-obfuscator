74da97665495774f9b0012cbc36fab1b2c2fbfe7
pmaslak <pawel+github@maslak.it>
2019-03-31 18:16:14 +0800

    core denture

---
+++ src/Core.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Pawel Maslak <pawel@maslak.it>
+ */
+
+namespace pmaslak\PhpObfuscator;
+
+
+class Core
+{
+    public function obfuscateFile(string $fileName, $configuration = [])
+    {
+
+    }
+}
