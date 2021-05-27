f4bc52bfcf8ad98e8e656e8b70058e20ebfb7041
mtouellette <77415006+mtouellette@users.noreply.github.com>
2021-05-27 10:44:34 -0400

    Change curly braces to brackets for offset access
    syntax is no longer support in PHP 8

--- src/MyPrettyPrinter.php
+++ src/MyPrettyPrinter.php
@@ -21,7 +21,7 @@ class MyPrettyPrinter extends PhpParser\PrettyPrinter\Standard
         $l = strlen($str);
         $result = '';
         for ($i = 0; $i < $l; ++$i) {
-            $result .= mt_rand(0, 1) ? "\x" . dechex(ord($str{$i})) : "\\" . decoct(ord($str{$i}));
+            $result .= mt_rand(0, 1) ? "\x" . dechex(ord($str[$i])) : "\\" . decoct(ord($str[$i]));
         }
         return $result;
     }
